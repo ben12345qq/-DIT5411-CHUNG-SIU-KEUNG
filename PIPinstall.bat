d7df39ef69ebc41cac26cd866ebe7eb583d7b48c
ben12345qq <ben12345qq71@gmail.com>
2025-11-17 09:29:56 +0800

    Add files via upload

---
+++ PIPinstall.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Installing required packages for the assignment...
+pip install tensorflow
+pip install keras
+pip install opencv-python
+pip install matplotlib
+pip install numpy
+echo Installation complete.
+pause
